51c301475975bac6d4323dbbd667d789cdc018de
antoineludwig <antoine.ludwig60@gmail.com>
2014-11-20 23:08:08 +0100

    Ajout de Personnages/Chateau.cs et Personnages/Ennemi.cs et de la FabriqueJeuDT.cs

---
+++ ProjetDesignPattern/ProjetDesignPattern/JeuDefenceTower/FabriqueJeuDT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDesignPattern.JeuDefenceTower
+{
+    class FabriqueJeuDT : FabriqueAbstraite
+    {
+        public abstract override PersonnageAbstrait CreerPersonnage(FabriqueAbstraite.eTypePersonnage typePerso, SujetObserveAbstrait unEtatMajor, string unNom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract override ZoneAbstraite CreerZone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract override AccesAbstrait CreerAcces()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
