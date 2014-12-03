29b067958b4bbe37e426c1eb63151045b4ad3549
antoineludwig <antoine.ludwig60@gmail.com>
2014-12-03 14:01:06 +0100

    Modif perso AbstractClass/PersonnageAbstrait.cs

--- ProjetDesignPattern/ProjetDesignPattern/AbstractClass/PersonnageAbstrait.cs
+++ ProjetDesignPattern/ProjetDesignPattern/AbstractClass/PersonnageAbstrait.cs
@@ -10,6 +10,9 @@ namespace ProjetDesignPattern
         public SujetObserveAbstrait EtatMajor { get; set; }
         public ZoneAbstraite Position { get; set; }
         public EtatAbstrait etatCourant { get; set; }
+        public ComportementCombattreAbstrait ComportementCombattre { get; set; }
+        public ComportementSeDeplacerAbstrait ComportementSeDeplacer { get; set; }
+        public ComporterSeDefendreAbstrait ComporterSeDefendre { get; set; }
 
         public abstract void AnalyserSituation();
         public abstract void Execution();
