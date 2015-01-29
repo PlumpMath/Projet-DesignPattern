54e70efd7bc55bd25316e02c4d09eb9b2b8a3174
Vincent <moittie.vincent@gmail.com>
2015-01-29 22:10:29 +0100

    Fix deplacement pion

--- ProjetDesignPattern/ProjetDesignPattern/JeuEchecs/Personnages/Comportement/Deplacement/DeplacementPion.cs
+++ ProjetDesignPattern/ProjetDesignPattern/JeuEchecs/Personnages/Comportement/Deplacement/DeplacementPion.cs
@@ -7,27 +7,32 @@ namespace ProjetDesignPattern.JeuEchecs
 	{
 		public DeplacementPion (bool type)
 		{
-			if(type)
-				this.déplacements = new int[][] { new int[] {7}, new int[] {8}, new int[] {9} };
+			if (type)
+				this.déplacements = new int[][] { new int[] {8,4}, new int[] {8}, new int[] {8,6} };//, new int[] {8, 8} };
 			else
-			this.déplacements = new int[][] { new int[] {1}, new int[] {2}, new int[] {3} };
+				this.déplacements = new int[][] { new int[] {2,4}, new int[] {2}, new int[] {2,3} };//, new int[] {2, 2} };
 			this.déplacementInfinie = false;
 			this.déplacementLibre = false;
 
 		}
 
 		public override ZoneAbstraite accessible(ZoneAbstraite zone, int[] a){
-			if (zone.zonesAdjacentes.ContainsKey (a [0])) {
-				if (a [0] == 8 || a[0] == 2) {
-					return zone.zonesAdjacentes [a[0]].arrivée;
-				} else {
-					zone = zone.zonesAdjacentes [a [0]].arrivée;
-					if(zone.listePersonnages.Count > 0){
-						return zone;
-					}
-				}		
+			ZoneAbstraite tmp = zone;
+			for (int index = 0; index < a.Length; index++) {
+				if (!tmp.zonesAdjacentes.ContainsKey (a [index]))
+					return null;
+				else if (tmp.zonesAdjacentes [a [index]].accès)
+					tmp = tmp.zonesAdjacentes [a [index]].arrivée;
+				else
+					return null;
+			}
+			if (a.Length > 1 && tmp.listePersonnages != null && tmp.listePersonnages.Count > 0) {
+				return tmp;
+			} else if (a.Length == 1) {
+				return tmp;
 			}
 			return null;
+
 		}
     }
 }
