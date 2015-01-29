9a558201bcb91484bbb29cb1b01c610fe1b357a0
Vincent <moittie.vincent@gmail.com>
2015-01-29 22:36:19 +0100

    Add double déplacement pion

--- ProjetDesignPattern/ProjetDesignPattern/JeuEchecs/Personnages/Comportement/Deplacement/DeplacementPion.cs
+++ ProjetDesignPattern/ProjetDesignPattern/JeuEchecs/Personnages/Comportement/Deplacement/DeplacementPion.cs
@@ -5,15 +5,29 @@ namespace ProjetDesignPattern.JeuEchecs
 {
 	public class DeplacementPion : ComportementSeDeplacerJE
 	{
+		private bool firstDéplacement = true;
+		private bool type;
 		public DeplacementPion (bool type)
 		{
+			this.type = type;
 			if (type)
-				this.déplacements = new int[][] { new int[] {8,4}, new int[] {8}, new int[] {8,6} };//, new int[] {8, 8} };
+				this.déplacements = new int[][] { new int[] {8,4}, new int[] {8}, new int[] {8,6}, new int[] {8, 8} };
 			else
-				this.déplacements = new int[][] { new int[] {2,4}, new int[] {2}, new int[] {2,3} };//, new int[] {2, 2} };
+				this.déplacements = new int[][] { new int[] {2,4}, new int[] {2}, new int[] {2,3}, new int[] {2, 2} };
 			this.déplacementInfinie = false;
 			this.déplacementLibre = false;
 
+		}
+
+		public override void deplacer(ZoneAbstraite zone){
+			base.deplacer (zone);
+			if (firstDéplacement) {
+				if (type)
+					this.déplacements = new int[][] { new int[] {8,4}, new int[] {8}, new int[] {8,6}};
+				else
+					this.déplacements = new int[][] { new int[] {2,4}, new int[] {2}, new int[] {2,3}};
+				firstDéplacement = false;
+			}
 		}
 
 		public override ZoneAbstraite accessible(ZoneAbstraite zone, int[] a){
@@ -26,7 +40,10 @@ namespace ProjetDesignPattern.JeuEchecs
 				else
 					return null;
 			}
-			if (a.Length > 1 && tmp.listePersonnages != null && tmp.listePersonnages.Count > 0) {
+			if (a.Length == 2 &&  (a[1] == 8 || a[1] == 2)) {
+				return tmp;
+			}
+			if (a.Length > 1 && tmp.listePersonnages.Count > 0) {
 				return tmp;
 			} else if (a.Length == 1) {
 				return tmp;
